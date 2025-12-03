a02331cf587033e4dcb1c37f2c8651a13f15acd5
Udlei Nati <udlei@pm.me>
2025-12-03 20:04:33 +0000

    feat: idempotency-id to idempotency-key

--- kong/plugins/idempotency/handler.lua
+++ kong/plugins/idempotency/handler.lua
@@ -1,7 +1,7 @@
 local access = require "kong.plugins.idempotency.access"
 local response = require "kong.plugins.idempotency.response"
 local cache = require "kong.plugins.idempotency.cache"
-local prefix = 'kong-idempotency-plugin:'
+local prefix = 'kong-idempotency-plugin'
 
 local Idempotency = {
   VERSION = "0.1.0",
@@ -15,7 +15,7 @@ end
 
 function Idempotency:response(conf)
   local client = cache.connection(conf)
-  response.execute(conf, Idempotency.versa, prefix, client)
+  response.execute(conf, Idempotency.VERSION, prefix, client)
 end
 
 return Idempotency
